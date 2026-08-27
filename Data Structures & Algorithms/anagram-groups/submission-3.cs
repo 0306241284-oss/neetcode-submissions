7e8e7fa0e98eaa6905a6b07203dd6f0f7fbacd8a
H4K3 <0306241284@caothang.edu.vn>
2026-08-28 00:38:41 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string,List<string>> dc = new Dictionary<string,List<string>>();
+        foreach(string str in strs){
+            char[] chars = new char[26];
+            chars = str.ToCharArray();
+            Array.Sort(chars);
+            string dcKey = new string(chars);
+            if(!dc.ContainsKey(dcKey)){
+                dc[dcKey] = new List<string>();
+            }
+            dc[dcKey].Add(str);
+        }
+        List<List<string>> result = new List<List<string>>();
+        foreach(var key in dc.Keys){
+            result.Add(dc[key]);
+        }
+
+        return result;   
+    }
+}
