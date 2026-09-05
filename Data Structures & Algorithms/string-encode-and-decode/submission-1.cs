2d5ffe5977f3d6bfd1d4b2e9c9d1cd46186d0759
H4K3 <0306241284@caothang.edu.vn>
2026-09-06 02:12:25 +0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cs
@@ -0,0 +1,25 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder res = new StringBuilder();
+        foreach(var s in strs){
+            res.Append(s.Length).Append('#').Append(s);
+        }
+        return res.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> res = new List<string>() ;
+        int i = 0;
+        while(i < s.Length){
+            int j = i;
+            while(s[j] != '#')j++;
+            int length = int.Parse(s.Substring(i,j-i));
+            i = j + 1;
+            j = i + length;
+            res.Add(s.Substring(i,j-i));
+            i = j;
+        }
+        return res;
+   }
+}
