2a0873802866a2dee6e7150f2472e01239702056
H4K3 <0306241284@caothang.edu.vn>
2026-09-11 00:58:54 +0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+     int[] check = new int[26];
+     if(s.Length != t.Length) return false;
+     for(int i = 0; i < s.Length ; i++){
+        check[s[i]-'a']++;
+     }
+    for(int i = 0; i < s.Length ; i++){
+        check[t[i]-'a']--;
+     }
+     foreach(int num in check ){
+        if(num != 0) return false;
+     }
+     return true;
+    }
+}
